629a07813bced8732903853746169046bd2911ae
Nick <nick@monksoftware.co.uk>
2016-04-26 23:01:13 +0100

    Integrating bootstrap

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [org.omcljs/om "1.0.0-alpha30"]
+                 [cljsjs/bootstrap "3.3.6-0"]
                  [figwheel-sidecar "0.5.0-6" :scope "test"]])
--- src/cryptocoin/core.cljs
+++ src/cryptocoin/core.cljs
@@ -1,7 +1,8 @@
 (ns cryptocoin.core
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]))
+            [om.dom :as dom]
+            [cljsjs.bootstrap]))
 
 (defui Cryptocoin
   Object
@@ -11,3 +12,14 @@
 (def cryptocoin (om/factory Cryptocoin))
 
 (js/ReactDOM.render (cryptocoin) (gdom/getElement "app"))
+
+;;
+;; TODO:
+;; -  Manage css using sass or equivalent
+;; 
+;; - List the markets with the currency with the most activity listed first:
+;; --"Currency" "%Increase: Minute, 15 Mins, 30 Mins, Hour, 8 Hours, 24 Hours"
+;;
+
+
+
