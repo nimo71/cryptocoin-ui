1045b160ed0fab7ff1c2e70abe5ea68b0789190a
Nick <nick@monksoftware.co.uk>
2016-05-12 00:20:11 +0100

    Tag the repository with the version number on deployment

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -4,4 +4,5 @@ docker save -o cryptoquants-ui-$TAG.docker nimo71/cryptoquants-ui:$TAG
 tar -cvzf cryptoquants-ui-$TAG.docker.tgz cryptoquants-ui-$TAG.docker
 scp -v -i ~/.ssh/NickAws.pem cryptoquants-ui-$TAG.docker.tgz ec2-user@$AWS_HOST:~/
 ssh -v -i ~/.ssh/NickAws.pem ec2-user@$AWS_HOST "bash -s" -- < ./run-docker.sh $TAG
+git tag $TAG
 
