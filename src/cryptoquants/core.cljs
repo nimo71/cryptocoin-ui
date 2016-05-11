ca27516ca5fae4b330a8c96daf4d6c02332698f6
Nick <nick@monksoftware.co.uk>
2016-05-12 00:02:51 +0100

    Changed 'Home' menu to 'Markets'

--- src/cryptoquants/core.cljs
+++ src/cryptoquants/core.cljs
@@ -76,33 +76,12 @@
             (dom/ul #js {:className "nav navbar-nav"}
               (dom/li #js {:className "active"} 
                 (dom/a #js {:href "#"
-                            :onClick #(change-route this '[:app/markets _] %)} "Home"))
+                            :onClick #(change-route this '[:app/markets _] %)} "Markets"))
               (dom/li nil 
                 (dom/a #js {:href "#about"
                             :onClick #(change-route this '[:app/about _] %)} "About"))))))
       (let [{:keys [app/route route/data]} (om/props this)]
         ((route->factory (first route)) data)))))
-
-    ;; <nav id="nav" class="navbar navbar-inverse navbar-fixed-top">
-    ;;   <div class="container">
-    ;;     <div class="navbar-header">
-    ;;       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-    ;;         <span class="sr-only">Toggle navigation</span>
-    ;;         <span class="icon-bar"></span>
-    ;;         <span class="icon-bar"></span>
-    ;;         <span class="icon-bar"></span>
-    ;;       </button>
-    ;;       <a class="navbar-brand" href="#">Cryptoquants</a>
-    ;;     </div>
-    ;;     <div id="navbar" class="navbar-collapse collapse">
-    ;;       <ul class="nav navbar-nav">
-    ;;         <li class="active"><a href="#">Home</a></li>
-    ;;         <li><a href="#about">About</a></li>
-    ;;       </ul>
-    ;;     </div><!--/.nav-collapse -->
-    ;;   </div>
-    ;; </nav>
-
 
 (defmulti read om/dispatch)
 (defmulti mutate om/dispatch)
@@ -126,19 +105,6 @@
   [{:keys [state]} _ {:keys [route]}]
   {:value {:keys [:app/route]}
    :action #(swap! state assoc :app/route route)})
-
-;; (defn read [{:keys [state] :as env} key params]
-;;   (let [st @state]
-;;     (if-let [[_ value] (find st key)]
-;;       {:value value}
-;;       {:value "not-found"})))
-
-;; (defn mutate [{:keys [state] :as env} key params]
-;;   (println "mutate, key: " key ", params: " params)
-;;   (if (= :table key)
-;;     {:value {:keys [:rows]}
-;;      :action #(swap! state assoc-in [:table :rows (key params)] (vals params))}
-;;     {:value :not-found}))
 
 (defn merge-market [state market] 
   (let  [currency-pair   (get market "currencyPair")
@@ -165,14 +131,4 @@
   (go-loop [market (<! ticker-chan)]
     (update-market market)
     (recur (<! ticker-chan))))
-
-
-;; TODO:
-;; -  Manage css using sass or equivalent
-;; 
-;; - List the markets with the currency with the most activity listed first:
-;; --"Currency" "%Increase: Minute, 15 Mins, 30 Mins, Hour, 8 Hours, 24 Hours"
-;;
-
-
 
